0d704a5db629f5f58a6d6db90714b8c8082af10f
Thann <nguyenthanhthang185@gmail.com>
2022-06-04 10:53:14 +0700

    fix exposed client secret id

--- src/services/send-email/google-config.ts
+++ src/services/send-email/google-config.ts
@@ -1,8 +1,9 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
+
+// Store CLIENT_SECRET_ID in .env to protect
 export enum GoogleConfig {
   CLIENT_ID = '1033208844589-64t3r56ivjti0q9if5lga56h3qrqgv10.apps.googleusercontent.com',
-  CLIENT_SECRET_ID = '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',
   REDIRECT_URI = 'https://developers.google.com/oauthplayground',
   REFRESH_TOKEN = `1//04gQevINxYZm9CgYIARAAGAQSNwF-L9Ir1Ds8tX51or371yjHPVsP707L-UPTI4ed3RTi2mGk8JN7Dpb9kUekUwrCu9MjyUNNZco`,
 }
