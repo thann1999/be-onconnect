c136767bd794a1fd57fd5fdd8332205df382a798
Nguyen Viet Dai <dai.nguyenviet@vti.com.vn>
2022-06-08 12:45:42 +0700

    change google config

--- src/services/send-email/google-config.ts
+++ src/services/send-email/google-config.ts
@@ -1,8 +1,8 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 export enum GoogleConfig {
-  CLIENT_ID = '1033208844589-64t3r56ivjti0q9if5lga56h3qrqgv10.apps.googleusercontent.com',
+  CLIENT_ID = '504571973119-r2j4fmk22nis7r1a9u6v9qssuu20aiqa.apps.googleusercontent.com',
   REDIRECT_URI = 'https://developers.google.com/oauthplayground',
-  REFRESH_TOKEN = `1//047o6deAkW9BECgYIARAAGAQSNwF-L9IrHc5c1SFHuqwaVuMTewwYdoSHScBAkOg_vingtEW--KUt8DLblG_31Zx9Po1iq5XdEO4`,
-  CLIENT_SECRET_ID = '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',
+  REFRESH_TOKEN = `1//04NrOdq-suwpGCgYIARAAGAQSNwF-L9IrZVC-P5baoMMKQQFNI549KI5bK_Kp3VzzKKATnvv1D2h7hPe6YC98B1bw-Gr_QClCzwA`,
+  CLIENT_SECRET_ID = '',
 }
